dd6c3174324c3140ef0a2ed4fd1cc330548e2903
Ashley Frank <ashley.frank@ufl.edu>
2023-05-26 17:48:21 -0400

    Added greet function and code

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+
+function greet (){
+
+    var name = prompt("What is your name?")
+    if (name.trim()){
+        alert('Hello, ' + name.trim() + '!')
+    }
+    else {
+        alert('Hello!')
+    }
+
+    var age = prompt("How old are you?")
+    var birthday = confirm("Did you have a birthday this year? (Click OK for yes, Cancel for no)")
+    if (birthday){
+       alert('You were born in ' + (new Date().getFullYear() - age) + '!')
+    }
+    else {
+        alert('You were born in ' + ((new Date().getFullYear() - age)-1) + '!')
+    }
+    
+}
